594a6612ee28e776eae478a2b6b826034b494d9e
David Benkeser <benkeser@emory.edu>
2024-05-16 11:22:08 +0300

    finish implementing fix for creating cloned data

--- code/create_cloned_data_set.R
+++ code/create_cloned_data_set.R
@@ -41,18 +41,20 @@ create_cloned_data_set <- function(
 	}
 
 	if("death_for_tb" %in% endpoint){
-		weekly_records_death_for_tb_z1 <- propensity_output$weekly_records_data[
-		  ((tpt_start_wk <= propensity_output$grace_pd_wks) & (wk <= min(death_wk, right_cens_wk_tb)) & (wk <= min(tb_wk, right_cens_wk_tb))) |
-	    ((tpt_start_wk > propensity_output$grace_pd_wks) & (wk <= propensity_output$grace_pd_wks) & (wk <= min(death_wk, right_cens_wk_tb)) & (wk <= min(tb_wk, right_cens_wk_tb)))
+		weekly_records_death_for_tb_z1 <- propensity_output$weekly_records_data[,
+		  .SD[((tpt_start_wk <= propensity_output$grace_pd_wks) & (wk <= pmin(death_wk, right_cens_wk_tb)) & (wk <= pmin(tb_wk, right_cens_wk_tb))) |
+	    		((tpt_start_wk > propensity_output$grace_pd_wks) & (wk <= propensity_output$grace_pd_wks) & (wk <= pmin(death_wk, right_cens_wk_tb)) & (wk <= pmin(tb_wk, right_cens_wk_tb)))],
+	    by = id
 	  ]
 	  weekly_records_death_for_tb_z1[, z := 1]
 	}
 
 	# for the "half" of the data set where z = 0
 	if("tb" %in% endpoint){
-		weekly_records_tb_z0 <- propensity_output$weekly_records_data[
-		  ((tpt_start_wk < 99999) & (wk <= tpt_start_wk) & (wk <= min(tb_wk, right_cens_wk_tb))) |
-		  ((tpt_start_wk == 99999) & (wk <= min(tb_wk, right_cens_wk_tb)))
+		weekly_records_tb_z0 <- propensity_output$weekly_records_data[,
+		  .SD[((tpt_start_wk < 99999) & (wk <= tpt_start_wk) & (wk <= pmin(tb_wk, right_cens_wk_tb))) |
+		  	  ((tpt_start_wk == 99999) & (wk <= pmin(tb_wk, right_cens_wk_tb)))],
+		  by = id
 		]
 		weekly_records_tb_z0[, z := 0]
 	}
@@ -66,9 +68,10 @@ create_cloned_data_set <- function(
 	}
 
 	if("death_for_tb" %in% endpoint){
-		weekly_records_death_for_tb_z0 <- propensity_output$weekly_records_data[
-		  ((tpt_start_wk < 99999) & (wk <= tpt_start_wk)) & (wk <= min(death_wk, right_cens_wk_tb)) & (wk <= min(tb_wk, right_cens_wk_tb)) |
-		  ((tpt_start_wk == 99999) & (wk <= min(death_wk, right_cens_wk_tb)) & (wk <= min(tb_wk, right_cens_wk_tb)))
+		weekly_records_death_for_tb_z0 <- propensity_output$weekly_records_data[,
+		  .SD[((tpt_start_wk < 99999) & (wk <= tpt_start_wk)) & (wk <= min(death_wk, right_cens_wk_tb)) & (wk <= min(tb_wk, right_cens_wk_tb)) |
+		  		((tpt_start_wk == 99999) & (wk <= min(death_wk, right_cens_wk_tb)) & (wk <= min(tb_wk, right_cens_wk_tb)))],
+		  by = id
 		]
 		weekly_records_death_for_tb_z0[, z := 0]
 	}
