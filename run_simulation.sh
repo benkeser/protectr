2232e969ccda5ff24b4d6e24fcb063af4a08d6a5
allicodi <allicodi@gmail.com>
2025-02-06 10:08:42 -0500

    file permissions

--- run_simulation.sh
+++ run_simulation.sh
@@ -11,8 +11,9 @@ export PATH=/apps/R/4.4.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 sbatch --partition=$PARTITION \
   --nodes=1 \
   --ntasks-per-node=1 \
-  --cpus-per-task=32 \
+  --cpus-per-task=31 \
   --mem-per-cpu=6G \
-  --job-name=tb_%a \
+  --job-name=tb_.%a \
+  --output=/projects/dbenkes/allison/protectr/scratch/%a_%J.out \
   --export=SETTING=$SETTING \
   --wrap "/apps/R/4.4.0/bin/Rscript run_simulation.R"
