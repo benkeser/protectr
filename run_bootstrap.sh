82ae92eac98aeb08aedf1059a049cfb864a7ce6d
Allison Codi <allison.codi@emory.edu>
2025-02-14 10:57:53 -0500

    4 task

--- run_bootstrap.sh
+++ run_bootstrap.sh
@@ -11,6 +11,7 @@ export PATH=/apps/R/4.4.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
  
 sbatch --array=1-$NSEEDS \
   --partition=$PARTITION \
+  -n=4 \
   --job-name=tb_hiv \
   --output=/projects/dbenkes/allison/protectr/scratch/${SETTING}_boot_${SLURM_ARRAY_TASK_ID}_%J.out \
   --export=SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID,SETTING=$SETTING \
