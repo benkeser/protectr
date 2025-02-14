26a880d101f07e98a53c2942eca79b188e6634f2
allicodi <allicodi@gmail.com>
2025-02-14 10:01:42 -0500

    mod for full run

--- run_bootstrap.sh
+++ run_bootstrap.sh
@@ -2,17 +2,16 @@
 
 PARTITION=$1
 SETTING=$2
+NSEEDS=$3
 
 module purge
 module load R/4.4.0
 
 export PATH=/apps/R/4.4.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
  
-sbatch --array=1 \
+sbatch --array=1-$NSEEDS \
   --partition=$PARTITION \
-  --cpus-per-task=1 \
-  --mem-per-cpu=10G \
   --job-name=tb_hiv \
-  --output=/projects/dbenkes/allison/protectr/scratch/${SETTING}_boot_%J.out \
+  --output=/projects/dbenkes/allison/protectr/scratch/${SETTING}_boot_${SLURM_ARRAY_TASK_ID}_%J.out \
   --export=SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID,SETTING=$SETTING \
   --wrap "/apps/R/4.4.0/bin/Rscript run_bootstrap.R"
