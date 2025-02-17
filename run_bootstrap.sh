bf6376457d6963f3c442ff9e25a3162dd644c58b
allicodi <allicodi@gmail.com>
2025-02-17 09:13:02 -0500

    successful version of bash script

--- run_bootstrap.sh
+++ run_bootstrap.sh
@@ -8,10 +8,14 @@ module purge
 module load R/4.4.0
 
 export PATH=/apps/R/4.4.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
- 
-sbatch --array=1-$NSEEDS \
+
+#--mem=0 request all mem on node
+
+sbatch --array=1-$NSEEDS%5 \
   --partition=$PARTITION \
-  -n=4 \
+  --nodes=1 \
+  --cpus-per-task=1 \
+  --mem=38000 \
   --job-name=tb_hiv \
   --output=/projects/dbenkes/allison/protectr/scratch/${SETTING}_boot_${SLURM_ARRAY_TASK_ID}_%J.out \
   --export=SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID,SETTING=$SETTING \
